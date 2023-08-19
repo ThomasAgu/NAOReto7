c4966e2439daaf0ec7503a9f6392bb52c5e16492
Thomas <thomas40651@gmail.com>
2023-08-18 21:13:25 -0300

    iquefhq

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,4 @@ export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '22081997';
 export const DB_NAME = process.env.DB_NAME || 'bookstore';
 export const DB_PORT = Number(process.env.DB_PORT) || 3306;
+export const DB_URL = process.env.DB_URL || '';
